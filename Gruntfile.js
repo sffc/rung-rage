f4aac7ad4db6f8e94124dcbe6902100a820b6fbf
Shane F. Carr <shane.carr@wustl.edu>
2016-07-18 22:42:24 -0700

    ES6 to ES5 transpilation for better browser support.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,8 @@
+// Tip: When developing, use the following commands to auto-compile the scripts when a change is detected.
+//
+// $ watchify html/main.js -o html/bundle.js -v &
+// $ watchify html/minimax.js -o html/bundle_minimax.js -v &
+
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON("package.json"),
@@ -8,8 +13,41 @@ module.exports = function(grunt) {
 					"html/bundle_minimax.js": ["html/minimax.js"]
 				}
 			}
+		},
+		babel: {
+			options: {
+				sourceMap: false,
+				presets: ["es2015"],
+				compact: false
+			},
+			dist: {
+				files: {
+					"html/bundle.js": "html/bundle.js",
+					"html/bundle_minimax.js": "html/bundle_minimax.js"
+				}
+			}
+		},
+		uglify: {
+			options: {
+				mangle: false,
+				sourceMap: false,
+				compress: {
+					dead_code: true
+				},
+				beautify: {
+					beautify: true
+				}
+			},
+			dist: {
+				files: {
+					"html/bundle.js": ["html/bundle.js"],
+					"html/bundle_minimax.js": ["html/bundle_minimax.js"]
+				}
+			}
 		}
 	});
 	grunt.loadNpmTasks("grunt-browserify");
-	grunt.registerTask("default", ["browserify"]);
+	grunt.loadNpmTasks("grunt-babel");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.registerTask("default", ["browserify", "babel", "uglify"]);
 };
